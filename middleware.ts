e0174b4bb0b4a02c23db800070f14caf015c774d
ardhendu66 <ardhenduroy2002@gmail.com>
2024-06-28 21:31:30 +0530

    bug fix

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
-// import type { NextRequest } from 'next/server';
-// import { NextResponse } from 'next/server';
-// import { envVariables } from './config/config';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { envVariables } from './config/config';
 
-// export { default } from 'next-auth/middleware';
+export { default } from 'next-auth/middleware';
 
-// export function middleware(req: NextRequest) {
-//     const { pathname } = req.nextUrl;
-//     if (pathname.startsWith(`/api/`)) {
-//         if (!req.headers.get("referer")?.includes(envVariables.domainUrl as string)) {
-//             return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-//         }
-//     }
+export function middleware(req: NextRequest) {
+    const { pathname } = req.nextUrl;
+    if (pathname.startsWith(`/api/`)) {
+        if (!req.headers.get("referer")?.includes(envVariables.domainUrl as string)) {
+            return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+        }
+    }
 
-//     return NextResponse.next();
-// }
+    return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ['/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)'],
-// }
+export const config = {
+  matcher: ['/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)'],
+}
